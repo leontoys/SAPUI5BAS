7d39f73e7a11a8ebfd8eb385ea7f90efdd77a69e
Liyon <svliyon@gmail.com>
2023-07-23 09:33:01 +1000

    added base controller

---
+++ 13-BaseController/controller/baseController.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+"sap/ui/core/mvc/Controller"
+], function(oController) {
+    'use strict';
+    return oController.extend("home.controller.baseController",{
+        oView,
+        onInit:function(){
+            this.oView = this.getView();
+        }
+    });
+});
--- 13-BaseController/controller/login.controller.js
+++ 13-BaseController/controller/login.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(oController) {
+    "sap/ui/core/mvc/Controller",
+    "home/controller/baseController"
+], function(oController,oBaseController) {
     'use strict';
-    return oController.extend("home.controller.login",{
-        oView,
-        onInit:function(){
-            this.oView = this.getView();
-        },
+    return oBaseController.extend("home.controller.login",{
+        //oView,
+        //onInit:function(){
+        //    this.oView = this.getView();
+        //},
         onLogin : function(){
+            console.log(oController);
+            console.log(oBaseController);
+            //alert("hello");
             var sUserName = this.oView.byId("inputUserName").getValue();
             var sPassword = this.oView.byId("inputPassword").getValue();
             if(sUserName=="admin"&&sPassword=="admin"){
