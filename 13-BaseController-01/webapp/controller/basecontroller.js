ef24aeefcb6bbd3ba75608b8132e8e6ba13ca750
Liyon SV <liyonblogs@gmail.com>
2023-07-29 23:53:12 +0000

    preview and base controller fixed

--- 13-BaseController-01/webapp/controller/basecontroller.js
+++ 13-BaseController-01/webapp/controller/basecontroller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], function(Controller) {
-    'use strict';
     return Controller.extend("home.basecontroller",{
         onInit:function(){
             alert("Hello from Base Controller");
--- 13-BaseController-01/webapp/controller/main.controller.js
+++ 13-BaseController-01/webapp/controller/main.controller.js
@@ -1,8 +1,7 @@
 sap.ui.define([
 "sap/ui/core/mvc/Controller",
-"home/basecontroller"
+"home/controller/basecontroller"
 ], function(Controller,BaseController) {
-    'use strict';
     return BaseController.extend("home.main",{
         onInit:function(){
             //alert("hello from Main controller");
