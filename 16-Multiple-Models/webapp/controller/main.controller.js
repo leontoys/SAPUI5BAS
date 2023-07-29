153945431d332b78a1fd4b6551120810fa973362
Liyon <svliyon@gmail.com>
2023-07-29 15:08:28 +1000

    on click of button enable and disable

--- 16-Multiple-Models/webapp/controller/main.controller.js
+++ 16-Multiple-Models/webapp/controller/main.controller.js
@@ -13,7 +13,8 @@ sap.ui.define([
                 "empId" : "100",
                 "empName" : "Alan",
                 "salary" : 10000,
-                "currency" : "AUD"
+                "currency" : "AUD",
+                "enabled" : false
             }
         });
         //set model to the core
@@ -21,6 +22,29 @@ sap.ui.define([
         //Binding-2 xml syntaxes are in XML, now let us look at JS syntaxes
         this.getView().byId("inputSalary").bindValue("/empStr/salary");
         this.getView().byId("inputCurrency").bindProperty("value","/empStr/currency");
+        //this.getView().byId("simpleForm").bindProperty("enabled","/empStr/enabled");
+        },
+
+        //on button click
+        onClick:function(){
+            //How to enable and disable based on button click?
+            //Method 1
+/*             var oEnabled = this.getView().byId("inputEmpId").getEnabled();
+            if(oEnabled){
+                sap.ui.getCore().getModel().setProperty("/empStr/enabled",false);
+            }
+            else{
+                sap.ui.getCore().getModel().setProperty("/empStr/enabled",true);
+            } */
+            //Method 2
+            var oModel = sap.ui.getCore().getModel();
+            var oEnabled = oModel.getProperty("/empStr/enabled");
+            if(oEnabled){
+                oModel.setProperty("/empStr/enabled",false);
+            }
+            else{
+                oModel.setProperty("/empStr/enabled",true);
+            }
         }
     });
 });
