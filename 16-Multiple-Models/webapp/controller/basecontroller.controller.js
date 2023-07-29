6262578ee5cabbb61e7593e4521ced93e118d190
Liyon <svliyon@gmail.com>
2023-07-29 14:33:44 +1000

    16-multiple-models

---
+++ 16-Multiple-Models/webapp/controller/basecontroller.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+"sap/ui/core/mvc/Controller"
+], function(oController) {
+    'use strict';
+    return oController.extend("home.controller.basecontroller",{
+        onInit:function(){
+            
+        }
+    });
+});
---
+++ 16-Multiple-Models/webapp/controller/main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+//"home/controller/basecontroller",
+"sap/ui/core/mvc/Controller"
+], function(oController) {
+    'use strict';
+    return oController.extend("home.controller.main",{
+        onInit:function(){
+
+        }
+    });
+});
