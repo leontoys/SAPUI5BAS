d22cd2c95badec700470cf5dd3e5b8d4b9458583
Liyon <svliyon@gmail.com>
2023-07-29 14:48:30 +1000

    added binding

--- 16-Multiple-Models/webapp/controller/main.controller.js
+++ 16-Multiple-Models/webapp/controller/main.controller.js
@@ -5,7 +5,22 @@ sap.ui.define([
     'use strict';
     return oController.extend("home.controller.main",{
         onInit:function(){
-
+            //create model 
+        var oModel = new sap.ui.model.json.JSONModel();
+        //set Data
+        oModel.setData({
+            "empStr":{
+                "empId" : "100",
+                "empName" : "Alan",
+                "salary" : 10000,
+                "currency" : "AUD"
+            }
+        });
+        //set model to the core
+        sap.ui.getCore().setModel(oModel);
+        //Binding-2 xml syntaxes are in XML, now let us look at JS syntaxes
+        this.getView().byId("inputSalary").bindValue("/empStr/salary");
+        this.getView().byId("inputCurrency").bindProperty("value","/empStr/currency");
         }
     });
 });
