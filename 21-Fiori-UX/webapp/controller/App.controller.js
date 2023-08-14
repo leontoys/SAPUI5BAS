ca23772dc676192354f7d0472747d6ce8df20358
liyon <svliyon@gmail.com>
2023-08-14 20:58:34 +1000

    day21 completed with fiori ux guidelines

---
+++ 21-Fiori-UX/webapp/controller/App.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+"sap/ui/demo/controller/BaseController"
+], function(BaseController) {
+    'use strict';
+    return BaseController.extend("sap.ui.demo.controller.App",{
+        
+    });
+});
