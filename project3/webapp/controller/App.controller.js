c911ba537ef44128861180dcb71417100ab916b8
Liyon SV <liyonblogs@gmail.com>
2024-02-12 07:46:29 +0000

    flexible column layout projects

---
+++ project3/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("demo.sap.com.project3.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ project3/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demo/sap/com/project3/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
